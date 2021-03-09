82e4d308afc0504172e3b056dbe953809d5e6377
433034 <433034@LTIN120789.cts.com>
2021-03-09 23:21:36 +0530

    create automation project

---
+++ E2ESeleniumjenkins/src/testcases/TC_001.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	@Test
+	public void startdriverinstance(){
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("hello");
+		driver.findElement(By.id("pass")).sendKeys("hello");
+		driver.findElement(By.name("login")).click();
+		driver.quit();
+	}
+	
+}
